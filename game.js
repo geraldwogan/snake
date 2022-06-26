b65e941b053cf6e495b3ea8dfab7501eb244cb08
geraldwogan <geraldwogan@gmail.com>
2022-06-26 18:05:45 +0100

    setup snake class

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
+import { update as updateSnake,  draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
 let lastRenderTime = 0
-const SNAKE_SPEED = 2 // how many times the snake moves per second
 
 function main(currentTime){
     window.requestAnimationFrame(main)
@@ -7,9 +8,18 @@ function main(currentTime){
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
     
-    window.requestAnimationFrame(main)
     lastRenderTime = currentTime
 
+    update()
+    draw()
 }
 
 window.requestAnimationFrame(main)
+
+function update(){
+    updateSnake()
+}
+
+function draw(){
+    drawSnake()
+}
